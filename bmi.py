68a15a9cc46a9f864d03544ecdcac683e0f42fec
zayanfm <izzzayan.24@ichat.sp.edu.sg>
2025-05-26 02:45:33 +0800

    added the return values

--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height, weight):
     
     if (bmi < 18.5): 
         print("underweight") 
+        return -1
     elif (bmi >= 18.5 and bmi <= 25.0): 
         print("normal weight") 
+        return 0
     else: 
         print("overweight")
+        return -1 
 
 calculate_bmi(1.73, 57)
 
